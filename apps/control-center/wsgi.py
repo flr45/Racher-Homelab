88720f2d01d4f91cf610d8187bfb06a70f3a882d
flr45 <151404060+flr45@users.noreply.github.com>
2026-07-28 22:55:29 +0200

    Register observability extension

--- apps/control-center/wsgi.py
+++ apps/control-center/wsgi.py
@@ -2,8 +2,10 @@ from app import create_app
 from configuration_extension import init_configuration_center
 from github_extension import init_github_center
 from maintenance_extension import init_maintenance
+from observability_extension import init_observability
 
 app = create_app()
 init_maintenance(app)
 init_github_center(app)
 init_configuration_center(app)
+init_observability(app)
