6ce7d33ba9521cd60ab1a81faa22ae98ab79e29f
flr45 <151404060+flr45@users.noreply.github.com>
2026-07-28 23:13:24 +0200

    Register Database Browser extension

--- apps/control-center/wsgi.py
+++ apps/control-center/wsgi.py
@@ -1,5 +1,6 @@
 from app import create_app
 from configuration_extension import init_configuration_center
+from database_browser_extension import init_database_browser
 from github_extension import init_github_center
 from maintenance_extension import init_maintenance
 from observability_extension import init_observability
@@ -9,3 +10,4 @@ init_maintenance(app)
 init_github_center(app)
 init_configuration_center(app)
 init_observability(app)
+init_database_browser(app)
