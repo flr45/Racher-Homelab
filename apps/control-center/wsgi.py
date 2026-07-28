7ed6945f3a060c61361de90c170d1ea8f0bf493b
flr45 <151404060+flr45@users.noreply.github.com>
2026-07-28 22:09:58 +0200

    Register configuration center

--- apps/control-center/wsgi.py
+++ apps/control-center/wsgi.py
@@ -1,7 +1,9 @@
 from app import create_app
+from configuration_extension import init_configuration_center
 from github_extension import init_github_center
 from maintenance_extension import init_maintenance
 
 app = create_app()
 init_maintenance(app)
 init_github_center(app)
+init_configuration_center(app)
