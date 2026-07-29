6fbd2c4efd67cade56c98abf8541224a1a35f804
flr45 <151404060+flr45@users.noreply.github.com>
2026-07-29 07:04:43 +0200

    Register Node & Hardware Center

--- apps/control-center/wsgi.py
+++ apps/control-center/wsgi.py
@@ -10,6 +10,7 @@ from logging_extension import init_structured_logging
 from maintenance_extension import init_maintenance
 from migration_extension import init_migrations
 from module_registry_extension import init_module_registry
+from node_hardware_extension import init_node_hardware_center
 from observability_extension import init_observability
 from operations_timeline_extension import init_operations_timeline
 from plugin_extension import init_plugin_center
@@ -38,4 +39,5 @@ init_docker_center(app)
 init_operations_timeline(app)
 init_ssh_console(app)
 init_readiness_center(app)
+init_node_hardware_center(app)
 init_module_registry(app)
